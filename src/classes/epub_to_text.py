e080334512373be810c81a3ce7ea46e48ec0e4eb
SedarOlmez94 <olmez49@gmail.com>
2026-08-10 20:49:09 +0100

    🎨 get metadata

--- src/classes/epub_to_text.py
+++ src/classes/epub_to_text.py
@@ -14,3 +14,30 @@ class EPUBToTextConverter:
 
     def __init__(self, epub_path: str) -> None:
         self.epub_path = epub_path
+
+
+    def get_ebook(self) -> epub.EpubBook:
+        """
+        Loads the EPUB file and returns an EpubBook object.
+
+        :return: An EpubBook object representing the loaded EPUB file.
+        """
+        book = epub.read_epub(self.epub_path)
+        return book
+
+    def get_metadata(self, book: epub.EpubBook) -> dict:
+        """
+        Extracts metadata from the EpubBook object.
+
+        :param book: An EpubBook object.
+        :return: A dictionary containing the metadata of the EPUB file.
+        """
+        metadata = {
+            "title": book.get_metadata("DC", "title")[0][0] if book.get_metadata("DC", "title") else "Unknown",
+            "author": book.get_metadata("DC", "creator")[0][0] if book.get_metadata("DC", "creator") else "Unknown",
+            "language": book.get_metadata("DC", "language")[0][0] if book.get_metadata("DC", "language") else "Unknown",
+            "publisher": book.get_metadata("DC", "publisher")[0][0] if book.get_metadata("DC", "publisher") else "Unknown",
+            "description": book.get_metadata("DC", "description")[0][0] if book.get_metadata("DC", "description") else "No description available.",
+        }
+        return metadata
+    
--- src/classes/main.py
+++ src/classes/main.py
@@ -5,10 +5,16 @@ Date: 09/08/2026
 """
 
 from pdf_to_text import PDFToTextConverter
+from epub_to_text import EPUBToTextConverter
 
 if __name__ == "__main__":
     pdf_converter = PDFToTextConverter("../../docs/test-paper.pdf")
     # text = pdf_converter.convert_to_text()
-    pdf_converter.build_book_from_pdf()
+    # pdf_converter.build_book_from_pdf()
 
-    print(pdf_converter.get_book(2))
+    # print(pdf_converter.get_book(2))
+
+    epub_object = EPUBToTextConverter("../../docs/shelley-frankenstein.epub")
+    epub_book = epub_object.get_ebook()
+    epub_metadata = epub_object.get_metadata(epub_book)
+    print(epub_metadata)
